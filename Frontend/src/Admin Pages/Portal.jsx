04ca706dce49607fa037cd2ed9f54a6e5b0649b3
Santosh Yada <ymanoutube@gmail.com>
2023-05-09 18:19:25 +0545

    SANTOSH:Admin Login Page Added

--- Frontend/src/Admin Pages/Portal.jsx
+++ Frontend/src/Admin Pages/Portal.jsx
@@ -1,47 +1,65 @@
-import React from "react";
+import React from 'react'
+import {useState} from 'react'
+import  Navigate  from 'react-router-dom'
 
 export default function Portal() {
-  const [secret, setSecret] = React.useState("");
+  
+  
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (secret === "1234") {
-      window.location.href = "/JobPosting";
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+
+  
+
+  const handleChange = (e) => {
+    if(e.target.type === "text"){
+      setUsername(e.target.value)
     }
-  };
+    else if(e.target.type === "password"){
+      setPassword(e.target.value)
+    }
+  }
+ 
+  const handleClick = (e) => {
+    e.preventDefault()
+    if(username === "admin" && password === "admin"){
+     Navigate("/ad");
+    }
+    else{
+      alert("Invalid Credentials")
+    }
+  }
+  
 
   const style = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "80vh",
-    width: "100%",
-    backgroundColor: "#f5f5f5",
-    color: "#000",
-    fontSize: "2rem",
-    fontWeight: "bold",
-    textAlign: "center",
-    margin: "1rem",
-    borderRadius: "1rem",
-    boxShadow: "0 0 10px rgba(0,0,0,0.5)",
-  };
+    display:"grid",
+    placeItems:"center",
+    height:"100vh",
+    fontFamily:"sans-serif",
+  }
+  const formStyle = {
+   boxShadow:"0 0 10px 0 rgba(0,0,0,0.2)",
+    padding:"2rem",
+    borderRadius:"10px",
+    
+  }
+  const inputStyle = {
+    width:"300px",
+  }
+
+
   return (
-    <div className="layout">
-      <div className="Portal-Page" style={style}>
-        <h1>Your Secret Code</h1>
-        <form action="none">
-          <input
-            type="text"
-            placeholder="Enter Your Secret Code"
-            value={secret}
-            onChange={(e) => setSecret(e.target.value)}
-          />
-          <button type="submit" onClick={handleSubmit}>
-            Lets Go
-          </button>
-        </form>
-      </div>
+    <div style={style}>
+      <form style={formStyle} >
+      <h1>Admin Portal</h1>
+        <input style={inputStyle} type="text" value={username}  onChange={handleChange}placeholder="Username"></input>
+        <input style={inputStyle} type="password" value={password} onChange={handleChange} placeholder="Password"></input>
+        <button type="submit" onClick={handleClick}>Login</button>
+      </form>
+
     </div>
-  );
+    
+
+  )
 }
